606cd57a955acb6383cf558c41954408c363e2f2
Efa-Iwa <gistend@gmail.com>
2022-06-02 21:18:39 -0700

    import functions that does some maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    '''print the sum, subtraction, multiple and division of 10 and 5'''
+    '''print the add, sub, mul and div of 10 and 5'''
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
