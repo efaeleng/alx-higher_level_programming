4455d3c56fb98f8e3f128e0a3ed16bfe652e2837
Efa-Iwa <gistend@gmail.com>
2022-06-09 00:21:20 -0700

    Tech interview prep: Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
-    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000}
     num = 0
 
     for i in range(len(roman_string)):
