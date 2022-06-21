b230f16b0d05c370771a364a785e3acdb875d689
Efa-Iwa <gistend@gmail.com>
2022-06-20 22:29:41 -0700

    divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         var = a / b
-    except zero_division_error:
+    except: ZeroDivisionError:
         var = None
     finally:
         print("Inside result: {}".format(var))
