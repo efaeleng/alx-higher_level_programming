e24b6b18ca05cced8d5add581b59bf262d7cc698
Efa-Iwa Eleng <gistend@gmail.com>
2022-07-05 23:40:57 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" Function that returns the list of available attributes
-        and methods of an object
-    """
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
-    """ Function that returns the list of available attributes
-    """
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
