e3551c766c6a42bfa5831d43a2173327e931f057
Efa-Iwa Eleng <gistend@gmail.com>
2022-07-06 05:46:57 +0100

    Create 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function that creates an Object from a JSON file
+"""
+import json
+
+def load_from_json_file(filename):
+    """ Function that creates an Object from a JSON file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
