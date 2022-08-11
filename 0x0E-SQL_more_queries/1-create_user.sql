b2b7236fdf529226154483165fff398d9725d0c3
Efa-Iwa <gistend@gmail.com>
2022-08-10 17:38:32 -0700

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
--- Creates the user user_0d_1 with all privileges on my server.
+-- Creates the user user_0d_1 with all privileges on the server.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   TO 'user_0d_1'@'localhost';
