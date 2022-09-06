b8016112ffb1829444c5edeaa6d07f763ed94a08
Efa-Iwa <gistend@gmail.com>
2022-09-05 20:29:31 -0700

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let sqr = '';
+    for (let j = 0; j < size; j++) {
+	row += 'X';
+    }
+    console.log(sqr);
+  }
+}
