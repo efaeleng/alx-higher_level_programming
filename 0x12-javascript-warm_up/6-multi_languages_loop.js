543f519a1fb81f5c46c68841895c0eebc8190f8e
Efa-Iwa <gistend@gmail.com>
2022-09-05 20:13:38 -0700

    Loop through languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (let myArray of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const myArray of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(myArray);
 }
